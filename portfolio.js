96641df7533203e8ff97adebe3e2e1dcf795a18c
yudaiukawa <55661117+yudaiukawa@users.noreply.github.com>
2021-03-13 07:08:21 +0900

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+$(function(){
+  $(".slider").slick({
+		fade:true,//切り替えをフェードで行う。初期値はfalse。
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 4000,//次のスライドに切り替わる待ち時間
+		speed:1000,//スライドの動きのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 1,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+		arrows: false,//左右の矢印あり
+});
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  $('.slider').slick('slickPlay');
+});
+});
